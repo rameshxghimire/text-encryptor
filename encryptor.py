1491371ec9f74f49daa793d426e5b13a5ffc6479
Ramesh Ghimire <rameshxghimire@gmail.com>
2018-12-21 14:44:32 +0200

    Encryptor function file added.

---
+++ encryptor.py
@@ -0,0 +1,44 @@
+"""
+ encryptor.py
+
+This is the main file that contains the encryption function.
+
+"""
+
+
+def encrypt(plaintext, key):
+    """
+    encrypt encryption function.
+
+    Encrypts the text using the Caesar Cypher method.
+
+    :param plaintext: text block input
+    :type plaintext: str
+    :param key: key for encryption
+    :type key: int
+    :return: text block output
+    :rtype: str
+    """
+    cyphertext = ''
+    for character in plaintext:
+        if character.isalpha():
+            number = ord(character)
+            number += key
+            if character.isupper():
+                if number > ord('Z'):
+                    number -= 26
+                elif number < ord('A'):
+                    number += 26
+            elif character.islower():
+                if number > ord('z'):
+                    number -= 26
+                elif number < ord('a'):
+                    number += 26
+            character = chr(number)
+        cyphertext += character
+
+    return cyphertext
+
+    # ! Test if the function works.
+    encrypt('Encrypts', 20)
+    print(cyphertext)
